f0e79f37c0163c49fc822171993d7992670ba2d0
AchmadDanniMPA <gundul321mmm@gmail.com>
2024-03-04 10:44:52 +0700

    Per 4 - Prak 3

---
+++ soal3-6.php
@@ -0,0 +1,7 @@
+<?php
+$totalKursi=45;
+$kursiDitempati=28;
+$kursiKosong=$totalKursi-$kursiDitempati;
+$persentaseKursiKosong=($kursiKosong/$totalKursi)*100;
+echo "Persentase kursi kosong di restoran: ".$persentaseKursiKosong."%";
+?>
